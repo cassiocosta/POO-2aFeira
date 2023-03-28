3a47009ba843b12ae40142d1f8e75f391aa746ca
Cássio h Costa <cassio@prouserapps.com>
2023-03-27 21:58:32 -0300

    aula 06

---
+++ aula06_associacoes/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using aula06_associacoes;
+
+Cerveja heineken = new Cerveja("334343434","ambev", 470,5);
+
+Console.WriteLine(heineken);
+
+
+Endereco end = new Endereco("");
+
+
+
