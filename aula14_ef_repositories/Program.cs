7842d7d6ed62454ba6939fe131e048649c1870ad
Cássio h Costa <cassio@prouserapps.com>
2023-05-15 19:27:07 -0300

    aulas

---
+++ aula14_ef_repositories/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using aula12_ef_test.Data;
+using aula12_ef_test.Domain;
+
+var db = new DataContext();
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+
+
+
+// Create
+Console.WriteLine("Inserting a new person");
+var person = new Person() { Id = 1, Name = "João da Silva", phoneNumber ="4454545454545" };
+db.Add(person);
+db.SaveChanges();
